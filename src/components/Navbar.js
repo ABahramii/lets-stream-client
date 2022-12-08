761dd4fb14a49a28b20b7bd5bf56f67abbd01c78
amir <amirbahraami99@gmail.com>
2022-12-08 15:31:23 +0330

    add Navbar components

---
+++ src/components/Navbar.js
@@ -0,0 +1,34 @@
+import "./navbar.css"
+import logoImage from "../images/dark-logo.png"
+import {Link} from "react-router-dom";
+
+export default function Navbar() {
+    return (
+        <nav className="navbar">
+            <div className="nav--list">
+                <Link to="/">
+                    <h3 className="logo">
+                        <img src={logoImage} alt="Site Logo"/>
+                        <span>Let's Stream</span>
+                    </h3>
+                </Link>
+            </div>
+
+            <div className="nav__links">
+                <Link to="/" className="nav__link">
+                    Lobby
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="#ede0e0" viewBox="0 0 24 24">
+                        <path d="M20 7.093v-5.093h-3v2.093l3 3zm4 5.907l-12-12-12 12h3v10h7v-5h4v5h7v-10h3zm-5 8h-3v-5h-8v5h-3v-10.26l7-6.912 7 6.99v10.182z"/>
+                    </svg>
+                </Link>
+                {/*Todo: route must be changed*/}
+                <Link to="/join" className="nav__link create__room__btn">
+                    Create Room
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="#ede0e0" viewBox="0 0 24 24">
+                        <path d="M12 0c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm6 13h-5v5h-2v-5h-5v-2h5v-5h2v5h5v2z"/>
+                    </svg>
+                </Link>
+            </div>
+        </nav>
+    );
+}
