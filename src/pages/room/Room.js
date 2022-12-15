fb25b9ee9e031be17f2f92828691146d7d32fd96
amir <amirbahraami99@gmail.com>
2022-12-15 11:35:24 +0330

    room: change style and structure

--- src/pages/room/Room.js
+++ src/pages/room/Room.js
@@ -1,8 +1,10 @@
 import "./room.css";
 import {useEffect, useState} from "react";
 import {useAuthContext} from "../../hooks/useAuthContext";
-import { over } from "stompjs"
-import SockJS from "sockjs-client"
+import { over } from "stompjs";
+import SockJS from "sockjs-client";
+
+import testImg from "../../images/dark-logo.png";
 
 let stompClient = null;
 
@@ -16,6 +18,7 @@ export default function Room() {
 
     useEffect(() => {
         if (user) {
+            console.log("hello register");
             registerUser();
         }
     }, []);
@@ -94,12 +97,12 @@ export default function Room() {
         console.log(err)
     }
 
-    const handleChatClick = (chat) => {
+    /*const handleChatClick = (chat) => {
         console.log(chat, Date.now());
         setTab(chat);
-    }
+    }*/
 
-    const handleSendPublicMessage = () => {
+    /*const handleSendPublicMessage = () => {
         if (stompClient) {
             let chatMessage = {
                 senderName: user.username,
@@ -110,9 +113,9 @@ export default function Room() {
             stompClient.send("/app/message", {}, JSON.stringify(chatMessage));
             setSendMessage("");
         }
-    }
+    }*/
 
-    const handleSendPrivateMessage = () => {
+    /*const handleSendPrivateMessage = () => {
         if (stompClient) {
             let chatMessage = {
                 senderName: user.username,
@@ -136,82 +139,116 @@ export default function Room() {
             stompClient.send("/app/private-message", {}, JSON.stringify(chatMessage));
             setSendMessage("");
         }
+    }*/
+
+    const handleKeyDown = (event) => {
+        event.preventDefault();
+        if (event.key === 'Enter') {
+            console.log(sendMessage);
+        }
     }
 
     return (
         <>
-            <header>
-                <h1>{user.username} </h1>
-            </header>
-
             { user &&
-                <main id="room_container">
-                    <section id="participants_container">
-                        <div id="participants_header">
-                            <p>Room Members</p>
-                            <p>{chatsCount}</p>
-                        </div>
-                        <div className="member_wrapper">
-                            <span className="green_dot"></span>
-                            <button onClick={() => handleChatClick("PUBLIC")}>ChatRoom</button>
-                        </div>
-                        {
-                            [...privateChats.keys()].map((name, index) => (
-                            <div key={index} className="member_wrapper">
-                                <span className="green_dot"></span>
-                                <button onClick={() => handleChatClick(name)}>{name}</button>
-                            </div>
-                            ))
-                        }
-                    </section>
+                <main className="container">
+                    <div id="room__container">
 
-                    {tab === "PUBLIC" &&
-                        <section id="chat_container">
-                            {
-                                publicChats.map((msg, index) => (
-                                    <div key={index} className="message-wrapper">
-                                        <small>{msg.date}</small>
-                                        <p>{msg.senderName}</p>
-                                        <p className="message">
-                                            {msg.message}
-                                        </p>
+                        {/* Todo: section 1 */}
+                        <section id="members__container">
+                            <div id="members__header">
+                                <p>Participants</p>
+                                <strong id="members__count">{chatsCount}</strong>
+                            </div>
+
+                            <div id="member__list">
+                                <div className="member__wrapper">
+                                    <span className="green__icon"></span>
+                                    <p className="member_name">{user.username}</p>
+                                </div>
+                                <div className="member__wrapper">
+                                    <span className="green__icon"></span>
+                                    <p className="member_name">afshin</p>
+                                </div>
+                                <div className="member__wrapper">
+                                    <span className="green__icon"></span>
+                                    <p className="member_name">james gosling</p>
+                                </div>
+                            </div>
+                        </section>
+
+                        {/* Todo: section 2*/}
+                        <section id="stream__container">
+                            <div id="stream__box"></div>
+
+                            <div id="streams__container"></div>
+
+                            <div className="stream__actions">
+                                <button id="camera-btn" className="active">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+                                        <path
+                                            d="M5 4h-3v-1h3v1zm10.93 0l.812 1.219c.743 1.115 1.987 1.781 3.328 1.781h1.93v13h-20v-13h3.93c1.341 0 2.585-.666 3.328-1.781l.812-1.219h5.86zm1.07-2h-8l-1.406 2.109c-.371.557-.995.891-1.664.891h-5.93v17h24v-17h-3.93c-.669 0-1.293-.334-1.664-.891l-1.406-2.109zm-11 8c0-.552-.447-1-1-1s-1 .448-1 1 .447 1 1 1 1-.448 1-1zm7 0c1.654 0 3 1.346 3 3s-1.346 3-3 3-3-1.346-3-3 1.346-3 3-3zm0-2c-2.761 0-5 2.239-5 5s2.239 5 5 5 5-2.239 5-5-2.239-5-5-5z"/>
+                                    </svg>
+                                </button>
+                                <button id="mic-btn" className="active">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+                                        <path
+                                            d="M12 2c1.103 0 2 .897 2 2v7c0 1.103-.897 2-2 2s-2-.897-2-2v-7c0-1.103.897-2 2-2zm0-2c-2.209 0-4 1.791-4 4v7c0 2.209 1.791 4 4 4s4-1.791 4-4v-7c0-2.209-1.791-4-4-4zm8 9v2c0 4.418-3.582 8-8 8s-8-3.582-8-8v-2h2v2c0 3.309 2.691 6 6 6s6-2.691 6-6v-2h2zm-7 13v-2h-2v2h-4v2h10v-2h-4z"/>
+                                    </svg>
+                                </button>
+                                <button id="screen-btn">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+                                        <path
+                                            d="M0 1v17h24v-17h-24zm22 15h-20v-13h20v13zm-6.599 4l2.599 3h-12l2.599-3h6.802z"/>
+                                    </svg>
+                                </button>
+                                <button id="leave-btn">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+                                        <path d="M16 10v-5l8 7-8 7v-5h-8v-4h8zm-16-8v20h14v-2h-12v-16h12v-2h-14z"/>
+                                    </svg>
+                                </button>
+                            </div>
+
+                            <button id="join-btn">Join Stream</button>
+                        </section>
+
+                        {/* Todo: section 3*/}
+                        <section id="messages__container">
+                            <div id="messages">
+                                {
+                                    publicChats.map((msg, index) => (
+                                        <div key={index} className="message__wrapper">
+                                            <img className="avatar__md" src="../../images/dark-logo.png"/>
+                                            <div className="message__body">
+                                                <strong className="message__author">{msg.senderName}</strong>
+                                                <small className="message__timestamp">{msg.date}</small>
+                                                <p className="message__text">{msg.message}</p>
+                                            </div>
+                                        </div>
+                                    ))
+                                }
+                                <div className="message__wrapper">
+                                    <img className="avatar__md" src={testImg} alt="world"/>
+                                    <div className="message__body">
+                                        <strong className="message__author">Amir</strong>
+                                        <small className="message__timestamp">9:33 AM</small>
+                                        <p className="message__text">hello world, front-end is not bad ! </p>
                                     </div>
-                                ))
-                            }
-                            <div>
+                                </div>
+                            </div>
+
+                            <form id="message__form">
                                 <input
                                     type="text"
                                     value={sendMessage}
                                     onChange={(event) => setSendMessage(event.target.value)}
+                                    onKeyDown={handleKeyDown}
+                                    placeholder="Send a message...."
                                 />
-                                <button onClick={handleSendPublicMessage}>send</button>
-                            </div>
-                        </section>
-                    }
+                            </form>
 
-                    {tab !== "PUBLIC" &&
-                        <section id="chat_container">
-                            {
-                                privateChats.get(tab).map((msg) => (
-                                    <div className="message-wrapper">
-                                        <p>{msg.senderName} - {msg.date}</p>
-                                        <p className="message">
-                                            {msg.message}
-                                        </p>
-                                    </div>
-                                ))
-                            }
-                            <div>
-                                <input
-                                    type="text"
-                                    value={sendMessage}
-                                    onChange={(event) => setSendMessage(event.target.value)}
-                                />
-                                <button onClick={handleSendPrivateMessage}>send</button>
-                            </div>
                         </section>
-                    }
-
+                    </div>
                 </main>
             }
         </>
