6df717258862adc0426878d10c582ef687acff96
amir <amirbahraami99@gmail.com>
2022-12-24 11:47:28 +0330

    add lobby page

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import Room from "./pages/room/Room";
-import Signing from "./components/Signing";
 import Join from "./pages/join/Join";
 import Navbar from "./components/Navbar";
+import Lobby from "./pages/lobby/Lobby";
 import Login from "./pages/login/Login";
 
 function App() {
@@ -11,10 +11,10 @@ function App() {
             <BrowserRouter>
                 <Navbar />
                 <Routes>
-                    <Route path="/" element={<Login />} />
+                    <Route path="/" element={<Lobby />} />
                     <Route path="/join" element={<Join />} />
                     <Route path="/room" element={<Room />} />
-                    <Route path="/s" element={<Signing />} />
+                    <Route path="/login" element={<Login />} />
                 </Routes>
             </BrowserRouter>
         </div>
---
+++ src/pages/lobby/Lobby.js
@@ -0,0 +1,7 @@
+export default function Lobby() {
+    return (
+        <div>
+
+        </div>
+    );
+}
