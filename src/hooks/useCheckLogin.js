5c2c16c31409befa02f9662b024db756260e116e
amir <amirbahraami99@gmail.com>
2023-01-05 12:05:12 +0330

    useCheckLogin: remove logs

--- src/hooks/useCheckLogin.js
+++ src/hooks/useCheckLogin.js
@@ -16,21 +16,18 @@ export const useCheckLogin = () => {
                 url: url,
                 method: "GET",
             }).then(res => {
-                // console.log(res);
                 const body = res.data;
                 setIsLogin(body.isTokenValid);
             }).catch(exp => {
                 setIsLogin(false);
             })
         } else {
-            // console.log("i have not this")
             setIsLogin(false);
         }
     }
 
 
     useEffect(() => {
-        // console.log("try to checkLogin");
         checkLogin();
     }, []);
 
