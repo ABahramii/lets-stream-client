4274ef43c14148cb84d293e669c894621457feb6
amir <amirbahraami99@gmail.com>
2023-02-04 20:01:40 +0330

    createRoom added

--- src/App.js
+++ src/App.js
@@ -4,8 +4,12 @@ import Join from "./pages/join/Join";
 import Navbar from "./components/Navbar";
 import Lobby from "./pages/lobby/Lobby";
 import Login from "./pages/login/Login";
+import CreateRoom from "./pages/room/CreateRoom";
+import checkLogin from "./service/checkLogin";
 
 function App() {
+    const isLogin = checkLogin();
+
     return (
         <div className="App">
             <BrowserRouter>
@@ -13,8 +17,9 @@ function App() {
                 <Routes>
                     <Route path="/" element={<Lobby />} />
                     <Route path="/join" element={<Join />} />
-                    <Route path="/room/:UUID" element={<Room />}/>
                     <Route path="/login" element={<Login />} />
+                    <Route path="/room/:UUID" element={<Room />}/>
+                    <Route path="/room/create" element={isLogin ? <CreateRoom /> : <Lobby />}/>
                 </Routes>
             </BrowserRouter>
         </div>
---
+++ src/pages/room/CreateRoom.js
@@ -0,0 +1,117 @@
+import "./createRoom.css"
+import {useState} from "react";
+import {useNavigate} from "react-router-dom";
+import useAuthRequest from "../../hooks/useAuthRequest";
+
+export default function CreateRoom() {
+    const [roomName, setRoomName] = useState("");
+    const [roomImage, setRoomImage] = useState(null);
+    const [imageName, setImageName] = useState("");
+    const [isPrivate, setIsPrivate] = useState(false);
+    const [privateCode, setPrivateCode] = useState("");
+
+    const navigate = useNavigate();
+    const [createRequest] = useAuthRequest();
+
+    const handleCreate = (event) => {
+        event.preventDefault();
+
+        const formData = new FormData();
+        formData.append('name', roomName);
+
+        formData.append('image', roomImage);
+        formData.append('active', true)
+        formData.append('privateRoom', isPrivate);
+
+
+        createRequest({
+            url: "/room/create",
+            method: 'POST',
+            data: formData
+        }).then(res => {
+            console.log("we have an error")
+            console.log(res)
+            // navigate('/dashboard')
+        }).catch(exp => {
+            console.log(JSON.stringify(exp))
+            // Todo: use toast
+        })
+
+    }
+    const addImage = (file) => {
+        setRoomImage(file);
+        setImageName(file.name);
+    }
+
+    return (
+        <>
+            <main id="room__lobby__container">
+                <div id="form__container">
+                    <div id="form__container__header">
+                        <p>Create Room</p>
+                    </div>
+
+                    <form id="lobby__form" onSubmit={handleCreate}>
+                        <div className="form__field__wrapper">
+                            <label>Room name</label>
+                            <label>
+                                <input type="text"
+                                       name="name"
+                                       onChange={e => setRoomName(e.target.value)}
+                                       value={roomName}
+                                       required
+                                       placeholder="Enter room name..."
+                                />
+                            </label>
+                        </div>
+
+                        <div className="wrapper">
+                            <div className="file-input">
+                                {/*Todo: check file type*/}
+                                <input
+                                    type="file"
+                                    id="file"
+                                    className="file"
+                                    required
+                                    aria-label="File browser example"
+                                    onChange={(e) => addImage(e.target.files[0])}
+                                />
+                                <label htmlFor="file">
+                                    Select image
+                                </label>
+                                <p className="file-name">{imageName}</p>
+                            </div>
+                        </div>
+
+                        <div className="wrapper">
+                            <label>private</label>
+                            <input
+                                type="checkbox"
+                                checked={isPrivate}
+                                onChange={() => setIsPrivate(!isPrivate)}
+                            />
+                        </div>
+
+                        {isPrivate &&
+                            <div className="form__field__wrapper">
+                                <label>Private code</label>
+                                <label>
+                                    <input type="text"
+                                           name="name"
+                                           onChange={e => setPrivateCode(e.target.value)}
+                                           value={privateCode}
+                                           placeholder="Enter room private code..."
+                                    />
+                                </label>
+                            </div>
+                        }
+
+                        <div className="form__field__wrapper">
+                            <button> Create </button>
+                        </div>
+                    </form>
+                </div>
+            </main>
+        </>
+    );
+}
