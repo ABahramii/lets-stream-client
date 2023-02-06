99609465263b5860fb79904d0f1fd535189e0fcf
amir <amirbahraami99@gmail.com>
2023-02-06 18:24:55 +0330

    Lobby: join completed

--- src/pages/lobby/Lobby.js
+++ src/pages/lobby/Lobby.js
@@ -3,58 +3,110 @@ import {useEffect, useState} from "react";
 import membersIcon from '../../images/members-icon.png'
 import checkLogin from "../../service/checkLogin";
 import useFetch from "../../hooks/useFetch";
+import {BASE_URL} from "../../config/Url";
+import authData from "../../data/authData";
+import {useNavigate} from "react-router-dom";
 
 
 export default function Lobby() {
 
-    const [fetchRoomRequest] = useFetch();
-
-
-    const [rooms, setRooms] = useState([
-        {
-            img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTk2ovDI44rCXwzd5gln9wHMO0z3Hsx2KB-8Q&usqp=CAUlPSqkM17H/AM+3+9/K1KlSqWI//9k=TpLk7pofiN1KlTXJm+DIlS40qVbnOUX6glPSqkM17H/AM+3+9/K1KlSqWI//9k=",
-            memberCount: 12,
-            name: "Let's stream",
-            owner: "amir bahrami"
-        },
-    ]);
+    const navigate = useNavigate();
+    const [request] = useFetch();
+    const [rooms, setRooms] = useState([]);
 
     useEffect(() => {
-        fetchRoomRequest({
-            url: `room/exists/`,
+        request({
+            url: "/room",
             method: "GET",
         }).then(res => {
-
+            const data = res.data;
+            if (data) {
+                setRooms(rooms.concat(data));
+            }
         }).catch(exp => {
             console.log(JSON.stringify(exp));
         })
     }, []);
 
+    const imageLink = (uuid) => {
+        return `${BASE_URL}/room/image/${uuid}`;
+    }
+
+    const joinRoom = (roomUUID) => {
+        let member = {};
+        let isLogin = checkLogin();
+
+        if (isLogin) {
+            request({
+                url: "/auth/token/isValid",
+                method: "POST",
+                data: {
+                    username: localStorage.getItem("username"),
+                    token: localStorage.getItem("token")
+                }
+            }).then(res => {
+                if (res.data.isTokenValid) {
+                    member.name = localStorage.getItem("username");
+                    member.user = true;
+                } else {
+                    onError();
+                }
+            }).catch(exp => {
+                onError();
+            })
+        } else {
+            navigate("/join")
+        }
+
+        request({
+            url: `room/${roomUUID}/checkJoin`,
+            method: "POST",
+            data: member
+        }).then(res => {
+            if (res.data.canJoin) {
+                navigate(`/room/${roomUUID}`);
+            } else {
+                // Todo: duplicate error with toast
+                console.log("user already exists at room");
+            }
+        }).catch(exp => {
+            console.log(JSON.stringify(exp));
+        })
+    }
+
+    const onError = () => {
+        authData.removeAuthData();
+        navigate("/login");
+        window.location.reload();
+    }
+
+    const handleJoin = (event, uuid) => {
+        event.preventDefault();
+        joinRoom(uuid);
+    }
+
     return (
         <div className="a1">
             <div className='a2'>
                 {
-                    rooms?.map((room) =>
-                        <div className='a3'>
+                    rooms.map((room, index) =>
+                        <div key={index} className='a3'>
                             <div className="a4">
-                                <img src={room.img} className="a5"/>
+                                <img src={imageLink(room.uuid)} className="a5"/>
                             </div>
                             <div className='a6'>
                                 <img src={membersIcon} className="a7"/>
-                                <div className="a8">Members count  {room.memberCount}</div>
+                                <div className="a8">{room.memberCount} watching</div>
                             </div>
                             <div className={'a9'}>
                                 {room.name}
                             </div>
                             <div className={'a10'}>
                                 <div className={'a11'}>{room.owner}</div>
-                                <div className={'a12'}>Join stream
-                                </div>
+                                <div className={'a12'} onClick={(event) => handleJoin(event, room.uuid)}>Join stream</div>
                             </div>
-
                         </div>
                     )
-
                 }
             </div>
         </div>
