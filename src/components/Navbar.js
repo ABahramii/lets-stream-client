78db0a0195a645f66ec2a346ef443d177062791b
amir <amirbahraami99@gmail.com>
2023-02-06 17:56:13 +0330

    Navbar: create room route changed

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -37,9 +37,8 @@ export default function Navbar() {
                         </svg>
                     </Link>
                 }
-                {/*Todo: route must be changed*/}
                 {isLogin &&
-                    <Link to="/join" className="nav__link create__room__btn">
+                    <Link to="/room/create" className="nav__link create__room__btn">
                         Create Room
                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="#ede0e0" viewBox="0 0 24 24">
                             <path d="M12 0c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm6 13h-5v5h-2v-5h-5v-2h5v-5h2v5h5v2z"/>
